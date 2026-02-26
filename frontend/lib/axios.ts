74b2b340e7225a052e77a3f3e6f7e236173296f9
Cannie511 <Canh51102@gmail.com>
2026-02-27 02:29:13 +0700

    fix env

--- frontend/lib/axios.ts
+++ frontend/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta?.env.MODE==='development' ? "http://localhost:2205/api" : "/api"
+const BASE_URL = process.env.MODE==='development' ? "http://localhost:2205/api" : "/api"
 
 export const api = axios.create({
     baseURL: BASE_URL,
